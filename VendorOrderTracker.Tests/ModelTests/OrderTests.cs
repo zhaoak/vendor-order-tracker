1672eb5a235184d659dff110b50bf4b4ef74602e
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-21 10:48:41 -0700

    Pass test for marking Order as delivered

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -26,11 +26,28 @@ public class OrderTests
     Assert.AreEqual(testOrder.Title, "Test Order");
     Assert.AreEqual(testOrder.Description, "Test Description");
     Assert.AreEqual(testOrder.Item, "Test Item");
-    // EntryDate needs to only be second-accurate
+    // EntryDate only needs to be second-accurate,
     // not millisecond-accurate
     Assert.AreEqual(testOrderEntryTime, testOrder.EntryDate.ToString());
     Assert.AreEqual(testOrderQuantity, testOrder.quantity);
     Assert.AreEqual(testOrderDollarPrice, testOrder.priceDollars);
     Assert.AreEqual(testOrderCentPrice, testOrder.priceCents);
+    Assert.AreEqual(false, testOrder.CheckIfDelivered());
+  }
+
+  [TestMethod]
+  public void Delivered_SetsDeliveredValue_True()
+  {
+    //Arrange
+    int testOrderQuantity = 5;
+    int testOrderDollarPrice = 10;
+    int testOrderCentPrice = 45;
+
+    //Act
+    Order testOrder = new Order("Test Order", "Test Description", "Test Item", testOrderQuantity, testOrderDollarPrice, testOrderCentPrice);
+    testOrder.Delivered();
+
+    //Assert
+    Assert.AreEqual(true, testOrder.CheckIfDelivered());
   }
 }

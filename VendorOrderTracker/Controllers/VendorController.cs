992c0a3fd1052dfe2f192b2e4f4eb395652a4f75
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-21 15:11:22 -0700

    Add Create page and route for Vendor

--- VendorOrderTracker/Controllers/VendorController.cs
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -6,10 +6,26 @@ namespace VendorOrderTracker.Controllers;
 
 public class VendorsController : Controller
 {
+  // vendor list page
   [HttpGet("/vendors")]
   public ActionResult Index()
   {
     List<Vendor> vendorList = Vendor.GetAll();
     return View(vendorList);
+  }
+
+  // add vendor form page
+  [HttpGet("/vendors/new")]
+  public ActionResult New()
+  {
+    return View();
+  }
+
+  // vender form submission route
+  [HttpPost("/vendors")]
+  public ActionResult Create(string vendorName, string vendorDescription)
+  {
+    Vendor newVendor = new Vendor(vendorName, vendorDescription);
+    return RedirectToAction("Index");
   }
 }

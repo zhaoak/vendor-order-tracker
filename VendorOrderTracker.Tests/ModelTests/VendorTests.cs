4d008ba8dce2aada1ca10905fa58b2f436adf491
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-28 17:29:03 -0700

    Add tests for Vendor.GetAll() and Vendor.Find()

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrderTracker.Models;
 using System;
+using System.Collections.Generic;
 
 namespace VendorOrderTracker.Tests;
 
@@ -28,5 +29,33 @@ public class VendorTests : IDisposable
     Assert.AreEqual(testVendorDescription, testVendor.Description);
     Assert.AreEqual(testVendor.Id, 0);
     Assert.AreEqual(testVendor2.Id, 1);
+  }
+
+  [TestMethod]
+  public void GetAll_ReturnsAll_Vendors()
+  {
+    List<Vendor> testVendorList = new List<Vendor> ();
+
+    Vendor testVendor1 = new Vendor("Shrek", "Ogre");
+    Vendor testVendor2 = new Vendor("Donkey", "Donkey");
+    testVendorList.Add(testVendor1);
+    testVendorList.Add(testVendor2);
+
+    CollectionAssert.AreEqual(testVendorList, Vendor.GetAll());
+  }
+
+  [TestMethod]
+  public void Find_FindsCorrect_Vendor()
+  {
+    //Arrange
+    Vendor testVendor1 = new Vendor("Shrek", "Ogre");
+    Vendor testVendor2 = new Vendor("Donkey", "Donkey");
+    Vendor testVendor3 = new Vendor("Fiona", "Ogre"); // will have ID of 2
+
+    //Act
+    Vendor foundVendor = Vendor.Find(2);
+    
+    //Assert
+    Assert.AreEqual(testVendor3, foundVendor);
   }
 }
